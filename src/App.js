8a39bfc94ab03a774e4883a3d8091713d9c2e379
finch <21374618@qq.com>
2019-05-16 14:50:22 +0800

    Changed mqtt websocket server address(intranet)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import './App.css';
+import {connect} from 'mqtt';
 
-init({
-    size:10000,
-    storageBackend: AsyncStorage,
-})
+var client = null;
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.handleMessage = this.handleMessage.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
         this.state = {
             message: '',
         };
     }
 
     componentDidMount() {
+        client = connect('alis://192.168.1.130:61614');
+        client.on('connect', err => {
+            if (!err) {
+                client.publish('mqtt/finch', 'Hello mqtt');
+            }
+        });
 
+        client.on('message', (topic, message) => {
+            console.log(message.toString());
+            // client.end();
+        });
+
+        client.subscribe('mqtt/finch', (err, granted) => {
+            if (!err) {
+                console.log(granted);
+                // client.end();
+            }
+        });
     }
 
     handleMessage(e) {
@@ -26,13 +44,23 @@ class App extends React.Component {
         });
     }
 
+    handleClick() {
+        console.info(client);
+
+
+
+        client.publish("mqtt/finch", this.state.message, e => {
+            console.info(e);
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <p>Hello, MQTT</p>
                 <input onChange={this.handleMessage}/>
 
-                <button onClick={this.send.bind(this)}>
+                <button onClick={this.handleClick}>
                     SendMessage
                 </button>
 
